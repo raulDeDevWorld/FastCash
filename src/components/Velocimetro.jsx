dc2121b9e128fbf2cd20f822f2c4191c0eac8241
Your Name <you@example.com>
2024-08-27 09:17:45 -0400

    DC

--- src/components/Velocimetro.jsx
+++ src/components/Velocimetro.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useRef, useState } from 'react';
-import Highcharts from 'highcharts';
+import * as Highcharts from 'highcharts';
 import HighchartsMore from 'highcharts/highcharts-more';
 import HighchartsReact from 'highcharts-react-official';
 
